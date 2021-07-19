11001723b7148cd473021bc2318beba09d5e4357
alvarogg777 <alvaro.gallego777@gmail.com>
2021-07-19 11:18:43 +0200

    init

---
+++ conda-recipes/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# install using pip from the whl file provided by Google
+pip install https://files.pythonhosted.org/packages/f8/11/8ce40b29b55b8a3b8e9dbd20423637cb56cfc5c1213d7ae1b33ec2095019/spektral-1.0.7-py3-none-any.whl
+
+# if [ `uname` == Linux ]; then
+#     if [ "$PY_VER" == "3.6" || "$PY_VER" == "3.7" || "$PY_VER" == "3.8" ]; then
+#         pip install https://files.pythonhosted.org/packages/f8/11/8ce40b29b55b8a3b8e9dbd20423637cb56cfc5c1213d7ae1b33ec2095019/spektral-1.0.7-py3-none-any.whl
+# fi
+
