c252c0d9c3a42c9d8181f287d13164a8aff84bb3
alborotogarcia <59288789+alborotogarcia@users.noreply.github.com>
2021-12-16 20:56:53 +0100

    bump spektral 1.0.8

--- conda-recipes/build.sh
+++ conda-recipes/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # install using pip from the whl file provided by Google
-pip install https://files.pythonhosted.org/packages/f8/11/8ce40b29b55b8a3b8e9dbd20423637cb56cfc5c1213d7ae1b33ec2095019/spektral-1.0.7-py3-none-any.whl
+pip install --no-cache-dir https://files.pythonhosted.org/packages/26/63/d756a1490d360d1cb398bb606eb282054e702e213ea983d3f76e99b83334/spektral-1.0.8-py3-none-any.whl
 
 # if [ `uname` == Linux ]; then
 #     if [ "$PY_VER" == "3.6" || "$PY_VER" == "3.7" || "$PY_VER" == "3.8" ]; then
